a177f4a3b3016490471dc2bb9a9b6dc293fbedfa
yuvlyg <yuvlyg@gmail.com>
2014-08-27 00:29:42 +0300

    added two servos control

--- sketch_aug04a/sketch_aug04a.ino
+++ sketch_aug04a/sketch_aug04a.ino
@@ -17,24 +17,33 @@ Arduino pins:
 
 #include <Servo.h> 
  
-Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
-int start_pos;
-int end_pos;
-SoftwareSerial BT(10, 11); // RX, TX
+Servo upper_servo;  // create servo object to control a servo 
+Servo lower_servo;
 
+SoftwareSerial BT(10, 11); // RX, TX
+  
 int KEY_PIN = 9; // set mode of the Bluetooth (AT or Pairing mode)
-int SERVO_PIN = 2;
+int U_SERVO_PIN = 2;
+int L_SERVO_PIN = 3; //TODO connect
 int LED_PIN = 12;
 
-int ON_POS = 160;
-int OFF_POS = 100;
-int servo_pos = OFF_POS;
+int U_ON_POS = 160;
+int U_OFF_POS = 100;
+int L_ON_POS = 160;
+int L_OFF_POS = 100;
+
+int upper_servo_pos = U_OFF_POS;
+int lower_servo_pos = L_OFF_POS;
+
+//enums
+int UPPER = 0;
+int LOWER = 1;
 
 void setup()  
 {
   
-  myservo.attach(SERVO_PIN);
+  upper_servo.attach(U_SERVO_PIN);
+  lower_servo.attach(L_SERVO_PIN);
   
   // Open serial communications and wait for port to open:
   Serial.begin(9600); // for AT mode
@@ -52,15 +61,18 @@ void setup()
   //digitalWrite(KEY_PIN, LOW); // Pairing mode
   
   delay(500);
-  move_servo(OFF_POS);
+  move_servo(UPPER, U_OFF_POS);
+  move_servo(LOWER, L_OFF_POS);
 }
 
 
 void handle_message(char message){
     if (message == '1'){
-      move_servo(ON_POS);
+      move_servo(UPPER, U_ON_POS);
+      move_servo(UPPER, U_OFF_POS);
     } else if (message == '0') {
-      move_servo(OFF_POS);
+      move_servo(LOWER, L_ON_POS);
+      move_servo(LOWER, L_OFF_POS);
     }
 }
 
@@ -79,15 +91,24 @@ void loop()
 }
 
 
-void move_servo(int dst_pos){
-  int diff = servo_pos > dst_pos ? -1 : 1;
-  for(int pos = servo_pos; pos != dst_pos; pos += diff)     // goes from cur_pos to dst_pos 
+void move_servo(int servo_id, int dst_pos){
+  Servo * servo;
+  int * pos;
+  if (servo_id == UPPER){
+    servo = &upper_servo;
+    pos = &upper_servo_pos;
+  } else {
+    servo = &lower_servo;
+    pos = &lower_servo_pos;
+  }
+  int diff = *pos > dst_pos ? -1 : 1;
+  for(; *pos != dst_pos; *pos += diff)     // goes from cur_pos to dst_pos 
   {
-    digitalWrite(LED_PIN, pos % 20 < 10 ? HIGH : LOW);    
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    digitalWrite(LED_PIN, *pos % 20 < 10 ? HIGH : LOW);    
+    servo->write(*pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
   }
-  servo_pos = dst_pos;
+
   digitalWrite(LED_PIN, HIGH);
 }
 
